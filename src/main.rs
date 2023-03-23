e652754961c69bb6b322465563f5a43de275bf89
Connor <connortalbott21@gmail.com>
2023-03-23 16:17:05 -0600

    websockets

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::Client;
+use tungstenite::{connect, Message as Messages};
+use url::Url;
 use serde::{Deserialize, Serialize};
 use image;
 use dotenv::dotenv;
+use lazy_static::lazy_static;
 
 use std::collections::HashMap;
-use std::sync::mpsc::channel;
 use std::time::Duration;
-use std::thread::sleep;
-use std::env;
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::env::var;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Message {
@@ -71,47 +74,37 @@ struct Code {
     stderr: String,
 }
 
-#[tokio::main]
-pub async fn fetch(bot: &str) -> Result<(), Box<dyn std::error::Error>> {
-    
-    send("Thank you for choosing to use bird bot! The bot will start in a couple of seconds.", bot, 1).await?;
+pub async fn fetch(channel: &str, msg: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut header = HeaderMap::new();
-    header.insert("x-bot-token", HeaderValue::from_str(bot)?);
+    header.insert("x-bot-token", HeaderValue::from_str(&BOT)?);
     
-    let url = "https://api.revolt.chat/channels/01GVVE0TZH01VB428E2NMF70J6/messages"; 
+    let url = format!("https://api.revolt.chat/channels/{}/messages", channel); 
 
-    loop {
-    //for _x in 0..1 {  
-        let client = Client::new().get(url).headers(header.clone()).send().await?.text().await?;
-        let message: Vec<Message> = serde_json::from_str(&client)?;
+    let client = Client::new().get(url).headers(header.clone()).send().await?.text().await?;
+    let message: Vec<Message> = serde_json::from_str(&client)?;
         
-        let contents = message.iter().map(|x| x.content.clone()).collect::<Vec<String>>();
+    let contents = message.iter().map(|x| x.content.clone()).collect::<Vec<String>>();
         
-        if contents[0].to_lowercase().starts_with("!eval") {
-            equation(contents[0].split("!eval").nth(1).unwrap_or(""), bot).await?;
-        }
-        
-        if contents[0].to_lowercase().starts_with("!comp"){
-            compile(contents[0].split("!comp").nth(1).unwrap_or(""), bot).await?;
-        }
-
-        if contents[0].to_lowercase().starts_with("!help"){
-            send("```# Commands: \n !eval: Evaluates a latex equation and responds with a picture of it. 
-            \n !comp (stable/beta/nightly, release/debug, 2021/2018/2015): Compiles rust code and responds with the output. If the parameters are not supplied they will be defulted to their first state.", bot, 1).await?;
-        }
-
-        if contents[0].to_lowercase().starts_with("!quit") {
-            send("Thanks for using bird-bot! Hope to see you soon!", bot, 1).await?;
-            break;
-        }
-
-        sleep(Duration::from_secs(5));
+    if contents[0].to_lowercase().starts_with("!eval") {
+        equation(contents[0].split("!eval").nth(1).unwrap_or(""), channel, msg).await?;
     }
+        
+    if contents[0].to_lowercase().starts_with("!comp"){
+        compile(contents[0].split("!comp").nth(1).unwrap_or(""), channel, msg).await?;
+    }
+
+    // Godbolt support
+
+    if contents[0].to_lowercase().starts_with("!help"){
+        send("```# Commands: \n !eval: Evaluates a latex equation and responds with a picture of it. 
+        \n !comp (stable/beta/nightly, release/debug, 2021/2018/2015): Compiles rust code and responds with the output. If the parameters are not supplied they will be defulted to their first state.".to_string(), channel, msg).await?;
+    }
+ 
     Ok(())
 }
 
-pub async fn compile(code: &str, bot: &str) -> Result<(), Box<dyn std::error::Error>>{
+pub async fn compile(code: &str, chan: &str, msg: &str) -> Result<(), Box<dyn std::error::Error>>{
     let mut split = code.splitn(2, "```rust");
     
     let left = split.next().unwrap().trim();
@@ -163,20 +156,20 @@ pub async fn compile(code: &str, bot: &str) -> Result<(), Box<dyn std::error::Er
     let response: Code = serde_json::from_str(&client)?;
 
     if response.success{
-        send(&response.stdout, bot, 2).await?;
+        send(format!("``` \n {}", &response.stdout), chan, msg).await?;
     }
     else{
-        send(&response.stderr, bot, 2).await?;
+        send(format!("``` \n {}", &response.stderr), chan, msg).await?;
     }
 
     Ok(())
 }
 
-pub async fn equation(equation: &str, bot: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn equation(equation: &str, channel: &str, msg: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut url = "https://latex.codecogs.com/png.latex?".to_string();
     url.push_str(&equation);
 
-    send(&url.replace(" ", ""), bot, 3).await?;
+    send(format!("[]({})", url.replace(" ", "")), channel, msg).await?;
     
     let client = Client::new().get(url).send().await?.bytes().await?;
     
@@ -186,23 +179,18 @@ pub async fn equation(equation: &str, bot: &str) -> Result<(), Box<dyn std::erro
     Ok(())
 }
 
-pub async fn send(text: &str, bot: &str, form: i32) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn send(text: String, channel: &str, message: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut header = HeaderMap::new();
-    header.insert("x-bot-token", HeaderValue::from_str(bot)?);
+    header.insert("x-bot-token", HeaderValue::from_str(&BOT)?);
 
     let mut map = HashMap::new();
 
-    if form == 3 {
-        map.insert("content", format!("[]({})", text));
-    }
-    else if form == 2 {
-        map.insert("content", format!("```{}```", text));
-    }
-    else if form == 1 {
-        map.insert("content", format!("{}", text));
-    }
+    map.insert("content", format!("{}", text));
 
-    let url = "https://api.revolt.chat/channels/01GVVE0TZH01VB428E2NMF70J6/messages"; 
+    // Im just going to leave this here incase I ever want to do it ...
+   // map.insert("replies", format!("[{}, true]", message));
+
+    let url = format!("https://api.revolt.chat/channels/{}/messages", channel); 
 
     let _client = Client::new().post(url).headers(header.clone()).json(&map).send().await?;
     //let body = client.text().await?;
@@ -210,9 +198,55 @@ pub async fn send(text: &str, bot: &str, form: i32) -> Result<(), Box<dyn std::e
     Ok(())
 }
 
+#[tokio::main]
+async fn socket() -> Result<(), Box<dyn std::error::Error>> {
+    let url = Url::parse("wss://ws.revolt.chat?version=1&format=json").unwrap();
+    let (mut socket, _) = connect(url).expect("Failed to connect");
+    
+    // Send auth request
+    let auth_request = format!("{{\"type\": \"Authenticate\", \"token\":\"{}\"}}", *BOT);
+    socket.write_message(Messages::Text(auth_request.into())).unwrap();
+
+    let paused = Arc::new(Mutex::new(false));
+
+    let message = r#"{"type": "Ping", "data": 0}"#;
+    socket.write_message(Messages::Text(message.into())).unwrap();
+
+    loop {
+        let msg = socket.read_message().unwrap();
+        
+        if let Messages::Text(msg_str) = &msg {
+            let parsed_msg: serde_json::Value = serde_json::from_str(msg_str).unwrap();
+            
+            if parsed_msg["type"] == "Pong" {
+                let paused_clone = paused.clone();
+                let sleeping_thread = thread::spawn(move || {
+                    *paused_clone.lock().unwrap() = true;
+                    thread::sleep(Duration::from_secs(15));
+                    *paused_clone.lock().unwrap() = false;
+                });
+                while *paused.lock().unwrap() {
+                    
+                }
+                sleeping_thread.join().unwrap();
+            }
+            else if parsed_msg["type"] == "Message" {
+                let msg = parsed_msg["_id"].as_str().unwrap();
+                let channel = parsed_msg["channel"].as_str().unwrap();
+                println!("{:?}", msg);
+                fetch(channel, msg).await?;
+            }
+        }
+        println!("{:?}", msg);
+    }
+}
+
 fn main(){
     dotenv().ok();
-    let seceret = env::var("BOT_KEY").unwrap();
 
-    fetch(&seceret).expect("A");
+    socket().expect("A");
+}
+
+lazy_static! {
+    static ref BOT: String = var("BOT_KEY").unwrap();
 }
